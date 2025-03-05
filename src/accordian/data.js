313fad91145849b3d8dace50ff0317eb1448f12c
ghareb <otmaneanna2020@gmail.com>
2025-03-05 23:07:27 +0000

    add folder random-color plus new file index.jsx

---
+++ src/accordian/data.js
@@ -0,0 +1,27 @@
+const data = [
+    {
+      id: "1",
+      question: "what are accordion components",
+      answer:
+        "Accordion componrnts are user interface elements used for organizing and presenting content in a collapsible manner. they typically consist of a header, content, and an expand/collapse action",
+    },
+    {
+      id: "2",
+      question: "what are they used for",
+      answer:
+        "They are commonly employed in various contexts, including FAQs, product descriptions, navigation menus, settings panels, and data tables, to save screen space and provide a stuctured and user-friendly interface for presenting information or options.",
+    },
+    {
+      id: "3",
+      question: "Accordion as a musical instrument",
+      answer:
+        "The accordion is a musical instrument with a keyboard and bellows, it produces sound by air passing over reds when the player expands or compresses the bellows, used in varios music generes",
+    },
+    {
+      id: "4",
+      question: "Can I create an accordion component with a different framework?",
+      answer: "Yes of course, it is very possible to create an accordion component with another framework",
+    },
+  ];
+  export default data;
+  
