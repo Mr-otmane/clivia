74d5494d6a58409655ab575b98416d0ef6d216e4
ghareb <otmaneanna2020@gmail.com>
2025-03-09 10:35:09 +0000

    add a folder name image-slider and two file index.jsx & styles.css in the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Accordian from "./accordian";
+import ImageSlider from "./components/image-slider";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 
@@ -11,7 +12,12 @@ function App() {
       {/* Random color component */}
       {/* <RandomColor/> */}
       {/* Star rating component */}
-      <StarRating noOfStars={10} />
+      {/* <StarRating noOfStars={10} /> */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
